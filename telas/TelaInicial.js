6d1e747e8d618072da82e60cd6e60d3e0cd8a261
Igor Costa <igor@tks.global>
2026-09-06 23:55:24 -0300

    feat: adaptacoes de texto, imagens e estilo na tela inicial

--- telas/TelaInicial.js
+++ telas/TelaInicial.js
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { View, Text, Image, Button, StyleSheet } from 'react-native';
+import { View, Text, Image, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { Jogo } from '../Globais';
 
@@ -16,16 +16,21 @@ export default function TelaInicial({ navigation }) {
     return (
         //Cria uma tela simples com um título, uma imagem e um botão que chama a primeira questão
         <View style={styles.container}>
-            <Text style={styles.titulo}>Bem-vindo ao Quiz!</Text>
+            <Text style={[styles.titulo, styles.textos]}>SAÚDE MENTAL: O que você sabe sobre o tema?</Text>
             <Image
-                source={require('../assets/logo.png')}
-                style={styles.logo}
+                source={require('../assets/capa.png')}
+                style={styles.capa}
             />
-            <Button
-                title="Iniciar Quiz"
+            <Text style={[styles.textos, styles.descricao]}>
+                Assim como nossa saúde física, cuidar da saúde mental também é essencial para nossa satisfação e qualidade de vida. Você sabe quais coisas são importantes para ter uma mente saudável? Faça o teste e descubra!
+            </Text>
+            <TouchableOpacity
+                
                 onPress={ () => navigation.navigate('Questao1')  }
                 style={styles.botao}
-           />
+           >
+                <Text style={[styles.textos, styles.textoBotao]}>Iniciar Quiz</Text>
+           </TouchableOpacity>
 
         </View>
     );
@@ -35,24 +40,48 @@ const styles = StyleSheet.create({
 
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
-        backgroundColor: '#1C2E3B',
+        backgroundColor: '#1C3040',
+        paddingTop: 50,
+    },
+
+    capa: {
+        width: '90%',
+        height: 350,
+        borderRadius: 25,
+        margin: 10,
+    },
+
+    textos: {
         color: '#fff',
+        textAlign: 'center',
         fontFamily: 'Roboto',
     },
 
     titulo: {
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: 'bold',
-        color: '#fff',
     },
 
-    logo: {
-        width: 200,
-        height: 200,
+    descricao: {
+        fontSize: 18,
+        padding: 20,
+        textAlign: 'justify'
     },
 
+    botao:{
+        width: 250,
+        height: 60,
+        backgroundColor: "#000",
+        justifyContent: 'center',
+        backgroundColor: '#01ADF1',
+        borderRadius: 12,
+    },
 
-
+    textoBotao:{
+        fontSize: 30,
+        fontWeight: 900,
+        color: '#1C3040'
+    },
 });
