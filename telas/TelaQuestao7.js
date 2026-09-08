2a8ca8a89a05d51e39edbee754b60598d6226aef
Igor Costa <igor@tks.global>
2026-09-07 23:04:14 -0300

    refactor: remove comentarios desnecessarios de TelaQuestao1 a TelaQuestao8

--- telas/TelaQuestao7.js
+++ telas/TelaQuestao7.js
@@ -19,8 +19,6 @@ export default function TelaQuestao1({ navigation }) {
             onProxima={
                 (pontos) => {
                     Jogo.pontuacao += pontos;
-                    //Aqui neste exemplo, como só temos uma questão, dela estou indo para a tela final. 
-                    //Quando você criar novas questões, vá para a Questão 2 e nas questões sempre para a próxima.
                     navigation.navigate('Questao8');
                 }
             }
