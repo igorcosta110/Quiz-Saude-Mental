94d7afaa25d7b257dc2f17a86808c9d9a01ecdad
Igor Costa <igor@tks.global>
2026-09-07 22:39:11 -0300

    refactor: remove comentarios e importacoes desnecessarias de TelaInicial.js

--- telas/TelaInicial.js
+++ telas/TelaInicial.js
@@ -1,17 +1,8 @@
-import React, { useCallback } from 'react';
-import { View, Text, Image, Button, StyleSheet, TouchableOpacity } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
-import { Jogo } from '../Globais';
+import React from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function TelaInicial({ navigation }) {
 
-    //Força que toda vez que a tela for reaberta (mesmo se já tiver sido criada e estiver na pilha)
-    // o valor de pontuação é zerado.
-    // useFocusEffect(
-    //     useCallback(() => {
-    //         Jogo.pontuacao = 0;
-    //     }, [])
-    // );
 
     return (
         //Cria uma tela simples com um título, uma imagem e um botão que chama a primeira questão
